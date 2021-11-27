ae9dbeb0cb3e46c4194b11b7e6a800b7b2ef7860
StefanSeliger <Your_seligerstefan84@gmail.com>
2021-11-27 17:19:34 +0100

    first upload

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+let ceasar=() => {
+    let x = ""
+    let code = document.getElementById("code")
+    let decode = document.getElementById("decode")
+    let textInput = document.getElementById("textInput")
+    let keyNumber = document.getElementById("keyNumber")
+    let output = document.getElementById("output")
+    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+    let newAlphabet = new Proxy(alphabet, {
+        get(target,prop) { //target=Array (alphabet) - prop=Property Elemente des Arrays
+            if(!isNaN(prop)) {
+                prop = parseInt(prop, 10)
+                if (prop < 0) {
+                    prop += target.length
+                }
+            }
+            return target[prop]
+        }
+    })
+
+    let inputSplit = textInput.value.toLowerCase().split("")
+
+    for (let i = 0; i < inputSplit.length; i++) {
+        for (let j = 0; j < alphabet.length; j++) {
+            if (inputSplit[i] == alphabet[j]) {
+                x += newAlphabet[j+Number(keyNumber.value)]
+                output.innerHTML = x
+            }
+        }
+    } 
+}
+ceasar()
